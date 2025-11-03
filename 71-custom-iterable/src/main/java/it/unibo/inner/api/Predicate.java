7fcbbbbf535438a172112a419f6c406f51bc6721
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-03 13:03:35 +0100

    Implement IterableWithPolicy and its iterator with filtering capabilities

--- 71-custom-iterable/src/main/java/it/unibo/inner/api/Predicate.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/api/Predicate.java
@@ -13,3 +13,63 @@ public interface Predicate<T> {
      */
     boolean test(T elem);
 }
+// package it.unibo.inner.impl;
+
+// import java.util.Iterator;
+// import java.util.NoSuchElementException;
+
+// import it.unibo.inner.api.IterableWithPolicy;
+// import it.unibo.inner.api.Predicate;
+
+// public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+    
+//     private final T[] elements;
+//     private Predicate<T> filter;
+
+//     public IterableWithPolicyImpl(final T[] elements) {
+//         this.elements = elements;
+//         this.filter = null;
+//     }
+
+//     public IterableWithPolicyImpl(final T[] elements, final Predicate<T> filter) {
+//         this.elements = elements;
+//         this.filter = filter;
+//     }
+
+//     @Override
+//     public Iterator<T> iterator() {
+//         return new IteratorWithPolicy();
+//     }
+
+//     @Override
+//     public void setIterationPolicy(final Predicate<T> filter) {
+//         this.filter = filter;
+//     }
+
+//     /**
+//      * Inner class that implements the Iterator interface with filtering policy.
+//      */
+//     private class  IteratorWithPolicy implements Iterator<T> {
+//         private int currentIndex = 0;
+
+//         @Override
+//         public boolean hasNext() {
+//             // Find the next element that satisfies the filter
+//             while (currentIndex < elements.length) {
+//                 if (filter == null || filter.test(elements[currentIndex])) {
+//                     return true;
+//                 }
+//                 currentIndex++;
+//             }
+//             return false;
+//         }
+
+//         @Override
+//         public T next() {
+//             if (!hasNext()) {
+//                 throw new NoSuchElementException();
+//             }
+//             return elements[currentIndex++];
+//         }
+//     }
+// }
---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,59 @@
+package it.unibo.inner.impl;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+    
+    private final T[] elements;
+    private Predicate<T> filter;
+
+
+    public IterableWithPolicyImpl(final T[] elements) {
+        this(elements, new Predicate<T>() {
+            @Override
+            public boolean test(T elem) {
+                return true;
+            }
+        });
+    }
+
+    public IterableWithPolicyImpl(final T[] elements, final Predicate<T> filter) {
+        this.elements = elements;
+        this.filter = filter;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new IteratorWithPolicy();
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        this.filter = filter;
+    }
+
+    private class IteratorWithPolicy implements Iterator<T> {
+
+        int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            while (currentIndex < elements.length) {
+                if (filter.test(elements[currentIndex])) return true;
+                currentIndex++;
+            }
+            return false;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return elements[currentIndex++];
+        }
+    }
+
+}
